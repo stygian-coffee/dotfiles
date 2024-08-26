bebd8c4be70a44fb74d1c1df6d8063c924bf3db5
Alexander Krigsman <a.krigsman@basenet.nl>
2024-08-26 13:56:31 +0200

    nvim: disable Treesitter gitcommit highlight

--- private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,4 +3,11 @@ return {
   -- For some reason, Treesitter sometimes fails to parse Jsonnet files if
   -- lazy loaded. Some kind of race condition I guess.
   lazy = false,
+  opts = {
+    highlight = {
+      enable = true,
+      -- gitcommit otherwise spits out a bunch of errors for some reason
+      disable = { "gitcommit" },
+    },
+  },
 }
